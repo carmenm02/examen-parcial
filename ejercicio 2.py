d04e87d5a6c39cccc19907d864497c2a60fc47c1
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-12-14 12:06:37 +0100

    he creado una funcion para crear otro tablero

--- ejercicio 2.py
+++ ejercicio 2.py
@@ -1,11 +1,13 @@
-import math 
-import os 
-import random 
-import re 
-import sys
-from typing import type_check_only
+from random import randint
 
 player1 = 0
 player2 = 0
+tablero = 0
 
-
+def funcion(filas, columnas):
+    if filas == 0 and tablero[filas + 1][columnas] != " ":
+        print("FALLO")
+    elif filas == 1 and tablero[filas + 1][columnas] != " ":
+        print("FALLO")
+    else:
+        print("No hay fallos")
