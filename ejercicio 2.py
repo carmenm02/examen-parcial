28288315600766e14cef060510700badc5815d13
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-12-14 12:10:46 +0100

    he creado una funcion para los movimientos de las fichas

--- ejercicio 2.py
+++ ejercicio 2.py
@@ -11,3 +11,18 @@ def funcion(filas, columnas):
         print("FALLO")
     else:
         print("No hay fallos")
+
+def movimientos(filas, columnas):
+    if filas == 0:
+        tablero[filas+1][columnas] = tablero[filas][columnas]
+        tablero[filas][columnas] = ' '
+    elif filas == 1:
+        if tablero[filas+1][columnas] != ' ':
+            tablero[filas-1][columnas] = tablero[filas][columnas]
+            tablero[filas][columnas] = ' '
+        else:
+            tablero[filas+1][columnas] = tablero[filas][columnas]
+            tablero[filas][columnas] = ' '
+    elif filas == 2:
+        tablero[filas-1][columnas] = tablero[filas][columnas]
+        tablero[filas][columnas] = ' '
