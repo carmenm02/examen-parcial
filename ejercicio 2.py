6845c79fe02f1e03fa65bbe74bb4b442bc89783e
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-12-14 11:15:50 +0100

    he creado un tablero, aunque todavia no se imprime bien

---
+++ ejercicio 2.py
@@ -0,0 +1,11 @@
+import math 
+import os 
+import random 
+import re 
+import sys
+
+def verticalRooks(r1,r2):
+    tablero = [1,2,3,4,5,6,7,8,9]
+    print(tablero[0],tablero[1],tablero[2])
+    print (tablero[3],tablero[4],tablero[5])
+    print(tablero[6],tablero[7],tablero[8])
